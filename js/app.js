6e68bede696477c40f5b6dafe71b609590dbcdef
Aloke Desai <aloke.desai@gmail.com>
2014-11-09 10:31:30 -0800

    remove all parse stuff; enable ability to add notes when you shave

--- js/app.js
+++ js/app.js
@@ -1,92 +1,31 @@
-var parseAPPID ="nWaGdXUhchnfgMeTeBC6d5pZZChOcEWM8Dl0FSwj";
-var parseJSID = "lvlI3tjmZ36VNA4dTTuilqJQCHwPyvRnocbvK4eI";
-
-//Initialize Parse
-Parse.initialize(parseAPPID,parseJSID);
-
-var NoteOb = Parse.Object.extend("Note");
-
-$(document).on("pageshow", "#home", function(e, ui) {
-	$.mobile.loading("show");
-
-	var query = new Parse.Query(NoteOb);
-	query.limit(10);
-	query.descending("createdAt");
-
-	
-	query.find({ 
-		success:function(results) {
-			$.mobile.loading("hide");
-			var $content = $("<div></div>");
-
-			// clean out the content
-			$(".list").html("");
-
-			console.log(results.length);
-			for(var i=0; i<results.length; i++) {
-				$innerContent = $("<li></li>");
-				var titleText;
-				if (results[i].get("title")) {
-					titleText = results[i].get("title");
-				} else {
-					titleText = "Untitled"
-				}
-
-				$title = $("<h1></h1>")
-						.text(titleText);
-				$hiddenContent = $("<p></p>")
-						.addClass("noteContent")
-						.text(results[i].get("text"));
-				
-				$innerContent.append($title);
-				$innerContent.append($hiddenContent);
-
-				$("#list").append($innerContent)
-				// $content.append($innerContent);
-			}
-
-			$("#list li").on("delete", function(evt) {
-  				console.log("sdf");
-			});
-		},error:function(e) {
-			$.mobile.loading("hide");
-
-		}
-	});
-});
-
-$(document).on("pageshow", "#addNote", function(e, ui) {
-
-	$("#saveNoteBtn").on("click", function(e) {
-		//e.preventDefault();
-		//$(this).attr("disabled","disabled").button("refresh");
+$("#saveNoteBtn").click(function() {
 
 		var noteText = $("#noteText").val();
 		var noteTitle = $("#noteTitle").val();
-		if(noteText === '') return;
+		if(noteText === '') {
+			return;
+		}
 
-			var note = new NoteOb();
-			note.set("text",noteText);
-			note.set("title", noteTitle);
-			note.save(null, {
-				success:function(ob) {
-					$.mobile.changePage("#home");
-				}, error:function(e) {
-					console.log("Oh crap", e);
-				}
-			});
-			cleanUp();
-	});
-	
-	function failHandler(e) {
-		alert("ErrorFromC");
-		alert(e);
-		console.log(e.toString());
-	}
+		// create li
+		$item = $("<li></li>");
+		$title = $("<h1></h1>").text(noteText);
+		$content = $("<p></p>").text(noteTitle);
+		$button = $("<div></div>").addClass("button remove").text("Remove");
+
+		$item.append($title);
+		$item.append($content);
+		$item.append($button);
+		$("#list").prepend($item);
+
+		// we need to readd trigger
+		$(".remove").click(function(){
+            $(this).parent().remove();
+        });
+
+		cleanUp();
+});
 
 	function cleanUp() {
 		$("#noteText").val("");
 		$("#noteTitle").val("");
 	}
-
-});
